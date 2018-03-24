8f54998dc5b0d85eb6dd9428a67224910f85958c
5-5-C <jmarshall.brock@gmail.com>
2018-03-24 15:46:46 -0400

    async I/O practice

---
+++ program.js
@@ -0,0 +1,76 @@
+//console.log("HELLO WORLD");
+
+//EXERCISE 2
+//console.log(process.argv);
+
+//var result = 0;
+//
+//for (var i = 2; i < process.argv.length; i++) {
+//    result += Number(process.argv[i]);
+//};
+//
+//    console.log(result);
+
+
+//Exercise 3
+
+//var fs = require('fs');
+//
+//var path = process.argv[2];
+//
+//var buffer = fs.readFileSync(path);
+//
+//var string = buffer.toString();
+//
+//var splitString = string.split('\n');
+//
+//var numOfLines = splitString.length - 1;
+//
+//console.log(numOfLines);
+
+
+
+
+//Exercise 4
+
+//var fs = require('fs');
+//
+//var path = process.argv[2];
+//
+//fs.readFile(path, function(err, data) {
+//    var numOfLines = data.toString().split('\n').length - 1;
+//    console.log(numOfLines);
+//})
+
+
+
+
+//Exercise 5
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
