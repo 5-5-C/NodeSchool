e5b4b1c1558bd5f7ea2981f915cdec5a485f4493
5-5-C <jmarshall.brock@gmail.com>
2018-03-26 21:47:09 -0400

    exercise 5&6 filters and modules

--- program.js
+++ program.js
@@ -49,10 +49,33 @@
 
 
 
+//var fs = require('fs')
+//var path = require('path')
+//
+//fs.readdir(process.argv[2], function (err, list) {
+//  list.forEach(function (file) {
+//    if (path.extname(file) === '.' + process.argv[3])
+//      console.log(file)
+//  })
+//})
 
 
 
+//Exercise 6
 
+
+var filterFn = require('./solution_filter.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+filterFn(dir, filterStr, function (err, list) {
+  if (err)
+    return console.error('There was an error:', err)
+
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
 
 
 
